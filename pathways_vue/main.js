53ad89140304cfc36765f3502186aa3e6cf00c56
Charlon Palacay <charlon@gmail.com>
2021-05-17 15:56:16 -0700

    Import all fontawesome icons for now.

--- pathways_vue/main.js
+++ pathways_vue/main.js
@@ -1,19 +1,27 @@
 import Vue from 'vue';
-import VueGtag from "vue-gtag";
+import VueGtag from 'vue-gtag';
 import VueMq from 'vue-mq';
 
-import {library} from '@fortawesome/fontawesome-svg-core';
+// fontawesome + vue-fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// load all icons for now... do treeshaking imports to optimize later
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(fas, far);
+
 import {
   FontAwesomeIcon,
   FontAwesomeLayers,
   FontAwesomeLayersText,
 } from '@fortawesome/vue-fontawesome';
 
+// bootstrap + bootstrap-vue
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
 
-import App from "./app.vue";
-import router from "./router";
-import store from "./store";
+import App from './app.vue';
+import router from './router';
+import store from './store';
 
 // custom theming and global styles
 import './css/custom.scss';
@@ -23,77 +31,7 @@ import './css/global.scss';
 const gaCode = document.body.getAttribute('google-analytics');
 const debugMode = document.body.getAttribute('django-debug');
 
-// fontaweome
-import {
-  faUser,
-  faLink,
-  faEnvelope,
-  faSearch,
-  faSignOutAlt,
-  faHome,
-  faPaw,
-  faBookmark,
-  faExclamationTriangle,
-  faGraduationCap,
-  faBars,
-  faLocationArrow,
-  faSquareFull,
-  faCaretRight,
-  faCaretDown,
-  faCaretUp,
-  faSquare as fasSquare,
-  faTimes,
-  faPencilAlt,
-  faCheck,
-  faPlus,
-  faCheckCircle,
-  faChevronRight,
-  faChevronUp,
-  faInfoCircle,
-} from '@fortawesome/free-solid-svg-icons';
-
-import {
-  faEdit,
-  faCreditCard,
-  faCalendarAlt,
-  faCalendarCheck,
-  faSquare,
-  faCircle,
-} from '@fortawesome/free-regular-svg-icons';
-
-library.add(faUser);
-library.add(faLink);
-library.add(faEnvelope);
-library.add(faSearch);
-library.add(faSignOutAlt);
-library.add(faHome);
-library.add(faPaw);
-library.add(faGraduationCap);
-library.add(faEdit);
-library.add(faCreditCard);
-library.add(faCalendarAlt);
-library.add(faCalendarCheck);
-library.add(faBookmark);
-library.add(faExclamationTriangle);
-library.add(faInfoCircle);
-library.add(faSquare);
-library.add(faSquareFull);
-library.add(fasSquare);
-library.add(faBars);
-library.add(faLocationArrow);
-library.add(faCaretRight);
-library.add(faCaretDown);
-library.add(faCaretUp);
-library.add(faTimes);
-library.add(faPencilAlt);
-library.add(faCheck);
-library.add(faPlus);
-library.add(faCheckCircle);
-library.add(faCircle);
-library.add(faChevronRight);
-library.add(faChevronUp);
-
-// fontawesome 5
+// vue setup
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.component('font-awesome-layers', FontAwesomeLayers);
 Vue.component('font-awesome-layers-text', FontAwesomeLayersText);
@@ -118,10 +56,10 @@ Vue.use(VueMq, {
     mobile: 767, // tablet begins 768px
     tablet: 991, // desktop begins 992px
     desktop: Infinity,
-  }
+  },
 });
 
-Vue.config.devtools = process.env.VUE_DEVTOOLS === "True";
+Vue.config.devtools = process.env.VUE_DEVTOOLS === 'True';
 
 export const dataBus = new Vue();
 
@@ -129,5 +67,5 @@ export const dataBus = new Vue();
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#main");
+  render: (h) => h(App),
+}).$mount('#main');
