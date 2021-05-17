885b71e90596c177d97e9837bd70f23ba48d7675
Charlon Palacay <charlon@gmail.com>
2021-05-17 13:57:59 -0700

    Initial import of Topbar layout component.

--- pathways_vue/layout.vue
+++ pathways_vue/layout.vue
@@ -1,32 +1,53 @@
 <template>
   <!-- layout.vue: this is where you override the layout -->
-  <div class="col-lg-8 mx-auto p-3 py-md-5" :app-name="appName" :page-title="pageTitle">
-    <header class="d-flex align-items-center pb-3 mb-5 h4 border-bottom">
-      <b-icon-front class="mr-2 text-purple"></b-icon-front>
-      <a href="/" class="text-reset text-decoration-none">{{ appName }}</a>
-    </header>
-
-    <h1>
-      <slot name="title">
-        {{ pageTitle }}
-      </slot>
-    </h1>
-
-    <slot name="description" />
-
-    <hr class="mb-5 w-25 d-inline-block" />
-
-    <slot name="content" />
-
-    <footer class="pt-5 my-5 text-muted border-top">
-      Copyright &copy; {{ currentYear }} University of Washington
-    </footer>
-  </div>
+  <topbar
+    :app-name="appName"
+    :page-title="pageTitle"
+    :user-name="userName"
+    :sign-out-url="signOutUrl"
+  >
+    <template #header></template>
+    <template #navigation>
+      <!-- navigation menu override -->
+      <b-nav vertical>
+        <b-nav-item
+          v-for="item in navItems"
+          :key="item.title"
+          class="mb-2"
+          :href="item.href"
+          :link-classes="'d-block px-2 py-1'"
+        >
+          {{ item.title }}
+        </b-nav-item>
+      </b-nav>
+    </template>
+    <template #main>
+      <!-- main section override -->
+      <h1>
+        <slot name="title">
+          {{ pageTitle }}
+        </slot>
+      </h1>
+      <slot name="content"></slot>
+    </template>
+    <template #footer>
+      <!-- footer section overrixe -->
+      <div class="text-white font-weight-light">
+        this is my custom footer<br />
+        Copyright &copy; {{ new Date().getFullYear() }} University of Washington
+      </div>
+    </template>
+  </topbar>
 </template>
 
 <script>
+import { Topbar } from 'axdd-components';
+
 export default {
-  components: {},
+  name: 'MyApp',
+  components: {
+    topbar: Topbar,
+  },
   props: {
     pageTitle: {
       type: String,
@@ -36,14 +57,33 @@ export default {
   data() {
     return {
       // minimum application setup overrides
-      appName: 'AXDD Django+Vue',
+      appName: 'MyApp',
+      userName: 'javerage',
+      signOutUrl: '/signout',
+
       // automatically set year
       currentYear: new Date().getFullYear(),
+
+      // example: dynamic navigation menu
+      navItems: [
+        {
+          title: 'Home',
+          href: '#',
+        },
+        {
+          title: 'First',
+          href: '#',
+        },
+        {
+          title: 'Second',
+          href: '#',
+        },
+        {
+          title: 'Third',
+          href: '#',
+        },
+      ],
     };
-  },
-  created: function() {
-    // constructs page title in the following format "Page Title - AppName"
-    document.title = this.pageTitle + ' - ' + this.appName;
   },
 };
 </script>
--- pathways_vue/pages/home.vue
+++ pathways_vue/pages/home.vue
@@ -1,64 +1,27 @@
 // home.vue
-
 <template>
   <layout :page-title="pageTitle">
     <!-- page content -->
-    <template #title>
-      {{ pageTitle }}
-    </template>
-
-    <template #description>
-      <p class="p-0 col-md-8 lead">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget pulvinar est. Sed
-        gravida neque justo, pellentesque finibus velit scelerisque eu. Curabitur et dolor luctus,
-        convallis magna at, mollis purus. Nunc tincidunt risus quis nisl bibendum volutpat. Praesent
-        ut tincidunt ex.
-      </p>
-    </template>
-
+    <template #title>{{ pageTitle }}</template>
     <template #content>
+      <div class="card mb-3 bg-dark hover:bg-danger">
+        <div class="card-body">asldkjfas</div>
+      </div>
       <div class="row">
-        <div class="col-md-6">
-          <h2>Development Stack</h2>
-          <p>
-            What's included? This template repository is intended to start you off with AXDD's
-            supported development stack.
-          </p>
-          <ul>
-            <li>Django (2.1 - 2.3)
-            <li>Vue (2.x)</li>
-            <li>Webpack (5.x)</li>
-            <li>Bootstrap (4.6.x)</li>
-          </ul>
+        <div class="col-md-4">
+          <div class="card mb-3 bg-dark">
+            <div class="card-body">asldkjfas</div>
+          </div>
         </div>
-        <div class="col-md-6">
-          <h2>AXDD Libraries</h2>
-          <p>
-           The following is a list of specific AXDD libraries used in building applications.
-          </p>
-          <ul>
-            <li><a href="https://github.com/uw-it-aca/django-container" target="_blank">Django Container</a></li>
-            <li><a href="https://github.com/uw-it-aca/django-webpack-bridge" target="_blank">Django Webpack Bridge</a></li>
-            <li><a href="https://github.com/uw-it-aca/axdd-components" target="_blank">AXDD Components</a></li>
-          </ul>
+        <div class="col-md-4">
+          <div class="card mb-3">
+            <div class="card-body">asldkjfas</div>
+          </div>
         </div>
-        <div class="col-md-6">
-          <h2>Vue Libraries</h2>
-          <p>
-           The following is a list of common Vue libraries included in this repository.
-          </p>
-          <ul>
-            <li><a href="https://bootstrap-vue.org/" target="_blank">BootstraVue</a></li>
-            <li><a href="https://github.com/FortAwesome/vue-fontawesome" target="_blank">Vue FontAwesome</a></li>
-            <li><a href="https://matteo-gabriele.gitbook.io/vue-gtag/" target="_blank">Vue Gtag</a></li>
-          </ul>
-        </div>
-        <div class="col-md-6">
-          <h2>Customization</h2>
-          <p>
-            Ready to go beyond the starter template and start buidling your application? Read about
-            how to <a href="/customize/">customize</a> your application.
-          </p>
+        <div class="col-md-4">
+          <div class="card mb-3 border-white hover:border">
+            <div class="card-body">asldkjfas</div>
+          </div>
         </div>
       </div>
     </template>
