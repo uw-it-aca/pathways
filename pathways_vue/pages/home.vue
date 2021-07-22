a7a32c1b46249be0ca7adfa64aad4daa361796ba
Diego <bexar@uw.edu>
2021-07-22 15:49:30 -0600

    home modal content and styles

--- pathways_vue/pages/home.vue
+++ pathways_vue/pages/home.vue
@@ -4,53 +4,77 @@
     <!-- page content -->
     <template #title>{{ pageTitle }}</template>
     <template #content>
+      <div class="row justify-content-center">
+        <div class="col-md-9">
+          <h1 class="visually-hidden">Pathways Home</h1>
 
-      <!-- Button trigger modal -->
-      <button
-        type="button"
-        class="btn btn-primary mb-5"
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-      >
-        Launch demo modal
-      </button>
+          <!-- Modal -->
+          <div
+            class="modal fade"
+            id="exampleModal"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog modal-dialog-centered modal-lg">
+              <div class="modal-content">
+                <div class="modal-body">
+                  <h2 class="modal-title mb-2" id="exampleModalLabel">Welcome</h2>
+                  <div>
+                    <p>
+                      This app helps with course planning and preparation for applying to a major. A
+                      few things to keep in mind before getting started:
+                    </p>
+                    <ul>
+                      <li>
+                        Grades are just one of the factors considered for admission to a
+                        capacity-constrained major. Reach out to your adviser to learn more.
+                      </li>
 
-      <!-- Modal -->
-      <div
-        class="modal fade"
-        id="exampleModal"
-        tabindex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog modal-dialog-centered modal-lg">
-          <div class="modal-content">
-            <div class="modal-body">
-              <h2 class="modal-title" id="exampleModalLabel">Modal title</h2>
-              <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
+                      <li>
+                        The median course grade and GPA data are only included for those students
+                        who had declared for the major (median grade for Chem 142, only includes
+                        those who declared Chemistry as their major).
+                      </li>
 
-              <div>asdfasdfasdf</div>
-
-              <button type="button" class="btn btn-purple" data-bs-dismiss="modal">
-                OK, got it
-              </button>
+                      <li>
+                        Discovering and applying for a major can be a challenging experience. Look
+                        for the “Contact your adviser” link to connect with your adviser.
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="text-end">
+                    <button
+                      type="button"
+                      class="btn btn-purple"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    >
+                      OK, got it
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
+
+          <ul>
+            <li><router-link to="/major">Go to Major</router-link></li>
+            <li><router-link to="/course">Go to Course</router-link></li>
+          </ul>
+
+          <search-chooser />
+          <!-- Button trigger modal -->
+          <button
+            type="button"
+            class="btn btn-link mb-5"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            About this app
+          </button>
         </div>
       </div>
-
-      <ul>
-        <li><router-link to="/major">Go to Major</router-link></li>
-        <li><router-link to="/course">Go to Course</router-link></li>
-      </ul>
-
-      <search-chooser />
-
     </template>
   </layout>
 </template>
@@ -64,8 +88,8 @@ import D3BoxPlot from '../components/d3-boxplot.vue';
 
 export default {
   components: {
-    'layout': Layout,
-    'search-chooser': SearchChooser
+    layout: Layout,
+    'search-chooser': SearchChooser,
   },
   data() {
     return {
@@ -76,4 +100,8 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+  .modal-body li {
+    margin-bottom:1em;
+  }
+</style>
