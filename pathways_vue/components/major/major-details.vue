35a59ca7ebdeb905443f41fc612df74ef1f5d1ed
Diego <bexar@uw.edu>
2021-07-23 10:33:02 -0600

    sample majordetails component

---
+++ pathways_vue/components/major/major-details.vue
@@ -0,0 +1,19 @@
+// major-details.vue 
+
+<template>
+    
+</template>
+
+
+<script>
+
+export default {
+  name: "MajorDetails",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss"></style>
