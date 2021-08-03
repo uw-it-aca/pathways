d1f3ceda004d51feb6f5b85600ef579f5bcedd4e
Charlon Palacay <charlon@gmail.com>
2021-08-03 13:47:37 -0700

    Get course data from prereqmap project.

--- pathways_vue/components/search/course.vue
+++ pathways_vue/components/search/course.vue
@@ -2,24 +2,38 @@
 
 <template>
   <div class="input-group">
-    <input type="text" class="form-control" aria-label="" placeholder="Enter a course code" />
+    <input
+      type="text"
+      list="courses"
+      v-model="course"
+      class="form-control"
+      aria-label=""
+      placeholder="Enter a course code"
+    />
     <button type="button" class="btn btn-purple" @click="searchCourse">Search</button>
   </div>
+  <datalist id="courses">
+    <option v-for="(course, i) in courses" :key="i">{{ course }}</option>
+  </datalist>
 </template>
 
 <script>
+import courseData from "./courses.json";
 
 export default {
-  name: "SearchMajor",
+  name: 'SearchMajor',
   data() {
-    return {};
+    return {
+      course: '',
+      courses: courseData
+    };
   },
   methods: {
     searchCourse(event) {
       // TODO: wire up submit for course search
-      alert('wire me up!')
-    }
-  }
+      alert('wire me up!');
+    },
+  },
 };
 </script>
 
