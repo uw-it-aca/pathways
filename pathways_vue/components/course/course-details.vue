fd3e0cd7ada307339bd0cf3141d1d67bfb7fbc8c
Diego <bexar@uw.edu>
2021-08-03 12:31:42 -0600

    adds explore-course and course-details comps

---
+++ pathways_vue/components/course/course-details.vue
@@ -0,0 +1,40 @@
+// course-details.
+
+<template>
+  <div class="mb-5">
+    <h2>CHEM 162: General Chemistry</h2>
+    <div><strong>Credits:</strong> 5</div>
+    <div>
+      <strong>Offered</strong>
+      <ul class="ms-2 d-inline list-inline">
+        <li class="list-inline-item"><span class="badge pw-green text-dark">SPR</span></li>
+        <li class="list-inline-item"><span class="badge creamcicle text-dark">AUT</span></li>
+        <li class="list-inline-item"><span class="badge bg-blue-200 text-dark">WIN</span></li>
+      </ul>
+    </div>
+
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque numquam a maxime delectus
+      tempora, ad molestiae rerum voluptate commodi cupiditate at quo fuga praesentium dicta
+      repellendus quia et accusantium ab
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CourseDetails',
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../css/custom.scss';
+
+.creamcicle {
+  background-color: $orange-200;
+}
+</style>
