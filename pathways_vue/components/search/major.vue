b50bbdb49fe4d3bff0e1a82bed3a1c48a02ec9bb
Charlon Palacay <charlon@gmail.com>
2021-08-04 12:05:40 -0700

    Add test major datalist.

--- pathways_vue/components/search/major.vue
+++ pathways_vue/components/search/major.vue
@@ -1,10 +1,22 @@
 // major.vue
-
 <template>
   <div class="input-group">
-    <input type="text" class="form-control" aria-label="" placeholder="Enter a major" />
+    <input type="text" class="form-control" aria-label="" placeholder="Enter a major" list="majors" />
     <button type="button" class="btn btn-purple" @click="searchMajor">Search</button>
   </div>
+  <datalist id="majors">
+    <option>American Ethnic Studies</option>
+    <option>Anthropology</option>
+    <option>Applied Mathematics</option>
+    <option>Communication</option>
+    <option>Economics</option>
+    <option>Philosophy</option>
+    <option>Physics</option>
+    <option>Bioresource Science &amp; Engineering</option>
+    <option>Computer Engineering</option>
+    <option>Environmental Health</option>
+    <option>Social Welfare</option>
+  </datalist>
 </template>
 
 <script>
