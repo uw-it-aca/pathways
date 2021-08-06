33e3aad57d7bdfe31f4c739382576505daf7d0e4
Diego <bexar@uw.edu>
2021-08-06 11:14:11 -0600

    adds prereq number badges

--- pathways_vue/components/course/prereq-course-list.vue
+++ pathways_vue/components/course/prereq-course-list.vue
@@ -1,9 +1,15 @@
 //prereq-course-list.vue
 
 <template>
-  <div class="card mb-5">
+  <div class="card mb-3">
     <div class="card-body">
-        <h4>Prerequesites <span class="badge badge-secondary">1</span></h4>
+        <h4>Prerequesites <span class="badge rounded-pill bg-secondary">1</span></h4>
+        <p>list here</p>
+    </div>
+  </div>
+  <div class="card mb-3">
+    <div class="card-body">
+        <h4>Courses Available Upon Completion <span class="badge rounded-pill bg-secondary">26</span></h4>
         <p>list here</p>
     </div>
   </div>
