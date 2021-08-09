3dafdebd7b9835993692b367609b40b8b263183c
Diego <bexar@uw.edu>
2021-08-09 10:48:17 -0600

    badge color and courses after completion list

--- pathways_vue/components/course/prereq-course-list.vue
+++ pathways_vue/components/course/prereq-course-list.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="card mb-3">
     <div class="card-body">
-      <h4>Prerequesites <span class="badge rounded-pill bg-secondary">1</span></h4>
+      <h4>Prerequesites <span class="badge rounded-pill bg-purple">1</span></h4>
       <div class="mt-3">
         <ul class="list-unstyled">
           <li>
@@ -25,9 +25,24 @@
   <div class="card mb-3">
     <div class="card-body">
       <h4>
-        Courses Available Upon Completion <span class="badge rounded-pill bg-secondary">26</span>
+        Courses Available Upon Completion <span class="badge rounded-pill bg-purple">26</span>
       </h4>
-      <p>list here</p>
+      <div class="mt-3">
+        <ul class="list-unstyled">
+          <li>
+            <a href="#" class="router-link-active"
+              ><span class="badge bg-link-color text-light">CSS 415</span></a
+            >
+            <span class="ms-3">Preparation for General Chemistry</span>
+          </li>
+          <li>
+            <a href="#" class="router-link-active"
+              ><span class="badge bg-link-color text-light">CSS 124</span></a
+            >
+            <span class="ms-3">Calculus with Analytic Geometry I</span>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
