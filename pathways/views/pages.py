55bdc96e491e57dd0f4b11c2adf1c5db64d37709
Jim Laney <jlaney@uw.edu>
2021-08-11 15:45:47 -0700

    pycodestyle fix

--- pathways/views/pages.py
+++ pathways/views/pages.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from django.views.generic import TemplateView
-import datetime
 
 
 class PageView(TemplateView):
@@ -11,7 +10,3 @@ class PageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
-
-    def render_to_response(self, context, **response_kwargs):
-        response = super(PageView, self).render_to_response(context, **response_kwargs)
-        return response
