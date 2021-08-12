3eaab298392af905329ef640e1b5f31a906724f9
Jim Laney <jlaney@uw.edu>
2021-08-12 12:46:36 -0700

    add login_required decorator

--- pathways/tests/test_views.py
+++ pathways/tests/test_views.py
@@ -2,13 +2,19 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from django.test import RequestFactory, TestCase
+from django.contrib.auth.models import User
+from django.contrib.sessions.middleware import SessionMiddleware
 from pathways.views import PageView
 
 
 class PagesViewTest(TestCase):
+    def setUp(self):
+        self.request = RequestFactory().get('/')
+        self.request.user = User()
+        SessionMiddleware().process_request(self.request)
+        self.request.session.save()
+
     def test_context(self):
-        factory = RequestFactory()
-        request = factory.get('/')
-        response = PageView.as_view()(request)
+        response = PageView.as_view()(self.request)
         self.assertIsInstance(response.context_data, dict)
         # Add tests for context values
--- pathways/views.py
+++ pathways/views.py
@@ -2,8 +2,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 
+@method_decorator(login_required, name='dispatch')
 class PageView(TemplateView):
     template_name = "index.html"
 
