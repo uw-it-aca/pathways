a4d81d8b452e27c3afc5af0f90474e9fe5a28d66
Diego <bexar@uw.edu>
2021-10-12 11:28:53 -0600

    updates app name

--- pathways_vue/components/major/contact-adviser-major.vue
+++ pathways_vue/components/major/contact-adviser-major.vue
@@ -6,7 +6,7 @@
       <h3>Are you considering this major?</h3>
       <p>
         We understand this can be a challenging process with many sources of information to
-        consider. This webpage is designed to be a useful tool but not a replacement for meeting
+        consider. DawgPath is designed to be a useful tool but not a replacement for meeting
         with an academic adviser. As you begin to consider what you want to major in, we recommend
         scheduling a meeting with your adviser to discuss your interests, goals, degree requirements
         and academic planning.
