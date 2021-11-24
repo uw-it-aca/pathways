ae2b955c5ce222247c22782f181b588614bf227e
Stephen De Vight <devights@uw.edu>
2021-11-24 14:04:03 -0800

    setting up user preference table and endpoint

---
+++ pathways/views/api/user.py
@@ -0,0 +1,26 @@
+# Copyright 2021 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from pathways.views.api import RESTDispatch
+from pathways.models.user import User
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from uw_saml.utils import get_user
+import json
+
+
+@method_decorator(login_required, name="dispatch")
+class UserPreference(RESTDispatch):
+    def post(self, request, *args, **kwargs):
+        uwnetid = get_user(self.request)
+        request_params = json.loads(request.body)
+        welcome_display = request_params.get("welcome_display", True)
+        defaults = {'uwnetid': uwnetid,
+                    'has_viewed_welcome': welcome_display}
+        user, created = \
+            User.objects.get_or_create(uwnetid=uwnetid,
+                                       defaults=defaults)
+        if created:
+            return self.json_response(status=200)
+        else:
+            return self.error_response(304)
