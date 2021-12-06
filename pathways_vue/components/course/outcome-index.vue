33d73156ebe59e11681f30d0ab776993e7e5ae0e
Diego <bexar@uw.edu>
2021-12-06 10:33:02 -0700

    better coi n/a logic

--- pathways_vue/components/course/outcome-index.vue
+++ pathways_vue/components/course/outcome-index.vue
@@ -28,20 +28,20 @@
             <i style="color: #ff8c00" class="bi bi-triangle-fill"></i>
             <span class="key-desc">{{course_id}}</span>
           </dt>
-          <dd v-if="course_coi == null" class="col-sm-6 key-coi">COI: Not Available</dd>
-          <dd v-else class="col-sm-6 key-coi">COI: {{course_coi}}</dd>
+          <dd v-if="course_coi" class="col-sm-6 key-coi">COI: {{course_coi}}</dd>
+          <dd v-else class="col-sm-6 key-coi">COI: Not Available</dd>
           <dt class="col-sm-6">
             <i class="bi bi-circle-fill"></i>
             <span class="key-desc">Average course in {{curric_abbr}} curriculum</span>
           </dt>
-          <dd v-if="curric_coi == null" class="col-sm-6 key-coi">COI: Not Available</dd>
-          <dd v-else class="col-sm-6 key-coi">COI: {{curric_coi}}</dd>
+          <dd v-if="curric_coi" class="col-sm-6 key-coi">COI: {{curric_coi}}</dd>
+          <dd v-else class="col-sm-6 key-coi">COI: Not Available</dd>
           <dt class="col-sm-6">
             <i class="bi bi-square-fill"></i>
             <span class="key-desc">Average {{course_level}} Level Course at UW</span>
           </dt>
-          <dd v-if="course_level_coi == null" class="col-sm-6 key-coi">COI: Not Available</dd>
-          <dd v-else class="col-sm-6 key-coi">COI: {{course_level_coi}}</dd>
+          <dd v-if="course_level_coi" class="col-sm-6 key-coi">COI: {{course_level_coi}}</dd>
+          <dd v-else class="col-sm-6 key-coi">COI: Not Available</dd>
         </dl>
         <p>*<!--{{percent_in_range}}--> <small>54% of all UW courses fall within the <!--{{range_text}}-->2 - 3 range.</small>
         </p>
