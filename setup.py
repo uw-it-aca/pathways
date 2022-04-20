4b6f75d4a444d8ef3a5605195c2c42a0b0ef9669
Jim Laney <jlaney@uw.edu>
2022-04-20 10:45:48 -0700

    update to django-container 1.4.0

--- setup.py
+++ setup.py
@@ -37,6 +37,5 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
     ],
 )
