176e66ab425f1cf44b5f317562052bdbca2a7239
Stephen De Vight <devights@gmail.com>
2022-05-20 12:26:03 -0700

    logging data import

--- docker/settings.py
+++ docker/settings.py
@@ -33,3 +33,74 @@ if os.getenv("ENV") == "localdev":
 
 LIMIT_USER_ACCESS = os.getenv('ENV') == 'eval'
 ALLOWED_USERS_GROUP = os.getenv("ACCESS_GROUP", default=None)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+
+        'add_user': {
+            '()': 'pathways.log.UserFilter'
+        },
+        'stdout_stream': {
+            '()': 'django.utils.log.CallbackFilter',
+            'callback': lambda record: record.levelno < logging.WARNING
+        },
+        'stderr_stream': {
+            '()': 'django.utils.log.CallbackFilter',
+            'callback': lambda record: record.levelno > logging.ERROR
+        }
+    },
+    'formatters': {
+        'pathways': {
+            'format': '%(levelname)-4s %(asctime)s %(user)s %(message)s [%(name)s]',
+            'datefmt': '[%Y-%m-%d %H:%M:%S]',
+        },
+    },
+    'handlers': {
+        'stdout': {
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+            'filters': ['add_user', 'stdout_stream'],
+            'formatter': 'pathways',
+        },
+        'stderr': {
+            'class': 'logging.StreamHandler',
+            'stream': sys.stderr,
+            'filters': ['add_user', 'stderr_stream'],
+            'formatter': 'pathways',
+        },
+        'pathways': {
+            'filters': ['add_user', 'stdout_stream'],
+            'formatter': 'pathways',
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+        },
+        'pathways_errors': {
+            'level': 'ERROR',
+            'filters': ['add_user', 'stderr_stream'],
+            'formatter': 'pathways',
+            'class': 'logging.StreamHandler',
+            'stream': sys.stderr,
+        },
+        'null': {
+            'class': 'logging.NullHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['stderr'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'pathways': {
+            'handlers': ['pathways', 'pathways_errors'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        '': {
+            'handlers': ['stdout', 'stderr'],
+            'level': 'INFO' if os.getenv('ENV', 'localdev') == 'prod' else 'DEBUG'
+        }
+    }
+}
