2d329acb8064b76620a8b3a6c746add9aa836fbc
Diego Bejarano <bexar@uw.edu>
2022-07-13 18:22:32 -0400

    adds bottleneck & gateway placeholders to faq page

--- pathways_vue/components/common/banner.vue
+++ pathways_vue/components/common/banner.vue
@@ -4,7 +4,7 @@
 
     <div class="alert alert-info alert-dismissible fade show text-center" role="alert">
         <strong>New Feature!</strong> DawgPath now shows which courses act as a bottleneck or a gateway for easier
-        registration planning. <a href="/faq#bottleneck-gateway" class="router-link">Learn more.</a>
+        registration planning. <a href="/faq#bottleneck" class="router-link">Learn more.</a>
         <button type="button" class="btn btn-link close" data-bs-dismiss="alert" aria-label="Close">
             <span aria-hidden="true"><i class="bi bi-x-lg" title="Dismiss alert"></i></span>
         </button>
