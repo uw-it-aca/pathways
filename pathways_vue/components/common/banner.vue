e18761df583f5ec1e7de87b39469c0e6b5db29c6
Diego Bexar <bexar@Diegos-MacBook-Pro.local>
2022-07-13 17:52:42 -0400

    adds new feature alert to layout

---
+++ pathways_vue/components/common/banner.vue
@@ -0,0 +1,31 @@
+// banner.vue
+
+<template>
+
+    <div class="alert alert-info alert-dismissible fade show text-center" role="alert">
+        <strong>New Feature!</strong> DawgPath now shows which courses act as a bottleneck or a gateway for easier
+        registration planning. <a href="#" class="/faq#bottleneck-gateway">Learn more.</a>
+        <button type="button" class="btn btn-link close" data-bs-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true"><i class="bi bi-x-lg" title="Dismiss alert"></i></span>
+        </button>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'Banner',
+    data() {
+        return {};
+    },
+    methods: {},
+    computed: {
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.alert {
+    border-radius: 0;
+}
+</style>
