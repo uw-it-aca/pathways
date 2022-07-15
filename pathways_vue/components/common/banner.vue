66ea3c537b6ae78c766fd2cf5453c48a8b8fb184
Stephen De Vight <devights@gmail.com>
2022-07-15 16:28:13 -0700

    adding bottleneck banner user pref

--- pathways_vue/components/common/banner.vue
+++ pathways_vue/components/common/banner.vue
@@ -2,10 +2,10 @@
 
 <template>
 
-    <div class="alert alert-info alert-dismissible fade show text-center mb-0" role="alert">
+    <div class="alert alert-info alert-dismissible fade show text-center mb-0" v-if="show_bottleneck">
         <strong>New Feature!</strong> DawgPath now shows which courses act as a bottleneck or a gateway for easier
         registration planning. <a href="/faq#bottleneck" class="router-link">Learn more.</a>
-        <button type="button" class="btn btn-link close" data-bs-dismiss="alert" aria-label="Close">
+        <button type="button" class="btn btn-link close" aria-label="Close" @click="dismissBottleneck">
             <span aria-hidden="true"><i class="bi bi-x-lg" title="Dismiss alert"></i></span>
         </button>
     </div>
@@ -16,10 +16,30 @@
 export default {
     name: 'Banner',
     data() {
-        return {};
+        return {show_bottleneck: false};
     },
-    methods: {},
+    methods: {
+      dismissBottleneck: function (){
+        this.saveModalPref();
+        this.show_bottleneck = false;
+      },
+      saveModalPref() {
+        this.axios({
+          method: 'post',
+          url: '/api/v1/user_pref/',
+          headers: { 'X-CSRFToken': window.csrf_token },
+          data: {
+            viewed_bottleneck_banner: true,
+          },
+        });
+      },
+    },
     computed: {
+    },
+    mounted() {
+      if (window.show_bottleneck) {
+        this.show_bottleneck = true;
+      }
     }
 };
 </script>
