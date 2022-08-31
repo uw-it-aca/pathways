8b22357481afbacbd9f5efd59e4c5ff064ca467c
Diego Bejarano <bexar@uw.edu>
2022-08-31 14:45:02 -0600

    resolves dawgpath-143

--- pathways_vue/components/common/banner.vue
+++ pathways_vue/components/common/banner.vue
@@ -3,8 +3,8 @@
 <template>
 
     <div class="alert alert-info alert-dismissible fade show text-center mb-0" v-if="show_bottleneck">
-        <strong>New Feature!</strong> DawgPath now shows which courses act as a bottleneck or a gateway for easier
-        registration planning. <a href="/faq#bottleneck" class="router-link">Learn more.</a>
+        <strong>New Feature!</strong> DawgPath now shows which courses act as a gateway course for easier
+        registration planning. <a href="/faq#gateway" class="router-link">Learn more.</a>
         <button type="button" class="btn btn-link close" aria-label="Close" @click="dismissBottleneck">
             <span aria-hidden="true"><i class="bi bi-x-lg" title="Dismiss alert"></i></span>
         </button>
