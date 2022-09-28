29eef775f4e201d8378aeb890ad8ca561363fbb1
Diego Bejarano <bexar@uw.edu>
2022-09-28 14:23:09 -0600

    program card styles

--- pathways_vue/components/major/explore-major.vue
+++ pathways_vue/components/major/explore-major.vue
@@ -5,25 +5,34 @@
   <div class="row mb-5">
     <div class="col-sm-4" v-if="major['program_code']">
       <div class="card">
-        <div class="card-body">
-          <h3 class="h6 card-title"><a class="nav-link p-0" :href="myplanProgramURL">Program Information</a></h3>
-          <small><p class="card-text p-0">Learn more about this program and its degrees</p></small>
+        <div class="card-body d-flex">
+          <div class="pe-2"><i class="bi bi-journal-bookmark display-5 text-purple"></i></div>
+          <div class="">
+            <h3 class="h6 card-title mb-0"><a class="nav-link p-0" :href="myplanProgramURL">Program Information</a></h3>
+            <small><p class="card-text p-0">Learn more about this program and its degrees</p></small>
+          </div>
         </div>
       </div>
     </div>
     <div class="col-sm-4" v-if="major['credential_code']">
       <div class="card">
-        <div class="card-body">
-          <h3 class="h6 card-title"><a class="nav-link p-0" :href="myplanCredentialURL">Degree Requirements</a></h3>
+        <div class="card-body d-flex">
+          <div class="pe-2"><i class="bi bi-clipboard-check display-5 text-purple"></i></div>
+          <div class="">
+          <h3 class="h6 card-title mb-0"><a class="nav-link p-0" :href="myplanCredentialURL">Degree Requirements</a></h3>
           <small><p class="card-text p-0">View courses needed to complete this degree</p></small>
+          </div>
         </div>
       </div>
     </div>
     <div class="col-sm-4" v-if="major['career_center_major']">
       <div class="card">
-        <div class="card-body">
-          <h3 class="h6 card-title"><a class="nav-link p-0" :href="careerCenterOutcomeURL">Career Outcomes</a></h3>
+        <div class="card-body d-flex">
+          <div class="pe-2"><i class="bi bi-pie-chart display-5 text-purple"></i></div>
+          <div class="">
+          <h3 class="h6 card-title mb-0"><a class="nav-link p-0" :href="careerCenterOutcomeURL">Career Outcomes</a></h3>
           <small><p class="card-text p-0">See what graduates do with this degree</p></small>
+          </div>
         </div>
       </div>
     </div>
