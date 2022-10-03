b532f03aaa70af20877f8475b618addcadd936f2
Charlon Palacay <charlon@gmail.com>
2022-10-03 15:23:06 -0700

    Updates readme. Fixes some eslint errors.

--- pathways_vue/router/index.js
+++ pathways_vue/router/index.js
@@ -1,31 +1,31 @@
 import { createWebHistory, createRouter } from "vue-router";
 
 // page components
-import Home from '../pages/home.vue';
-import Course from '../pages/course.vue';
-import Major from '../pages/major.vue';
-import Faq from '../pages/faq.vue';
+import Home from "../pages/home.vue";
+import Course from "../pages/course.vue";
+import Major from "../pages/major.vue";
+import Faq from "../pages/faq.vue";
 
 const routes = [
   {
     path: "/",
-    component: Home
+    component: Home,
   },
   {
     path: "/faq",
     component: Faq,
-    pathToRegexpOptions: { strict: true }
+    pathToRegexpOptions: { strict: true },
   },
   {
     path: "/course",
     component: Course,
-    pathToRegexpOptions: { strict: true }
+    pathToRegexpOptions: { strict: true },
   },
   {
     path: "/major",
     component: Major,
-    pathToRegexpOptions: { strict: true }
-  }
+    pathToRegexpOptions: { strict: true },
+  },
 ];
 
 const router = createRouter({
