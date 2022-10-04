a6dafc203b3e595494239b334df2bbcc278dc119
Diego Bejarano <bexar@uw.edu>
2022-10-04 16:27:12 -0600

    adds new career outcomes banner

--- pathways_vue/components/common/banner.vue
+++ pathways_vue/components/common/banner.vue
@@ -2,10 +2,8 @@
 
 <template>
 
-    <div class="alert alert-info alert-dismissible fade show text-center mb-0" v-if="show_outcomes">
-        <strong>New Feature!</strong> DawgPath now shows which courses act as a gateway. Look for the gateway icon  <div class="round round-sm bg-success">
-      <span class="material-symbols-outlined fw-bold">call_split</span>
-    </div>  in course listings.  <a href="/faq#gateway" class="router-link">Learn more.</a>
+    <div class="alert alert-primary alert-dismissible fade show text-center mb-0"> <!-- v-if="show_outcomes" -->
+        <strong>New Feature!</strong> What have recent UW graduates done with their degree? Check out the "<strong>Career Outcomes</strong>" link, available for most majors.
         <button type="button" class="btn btn-link close" aria-label="Close" @click="dismissOutcomes">
             <span aria-hidden="true"><i class="bi bi-x-lg" title="Dismiss alert"></i></span>
         </button>
