1d3117be96059ac9e6f7403a9f4977e88c3944d2
Stephen De Vight <devights@uw.edu>
2023-01-27 18:03:03 -0800

    updating tests

--- pathways/tests/test_api.py
+++ pathways/tests/test_api.py
@@ -27,6 +27,7 @@ class TestMajorApi(ApiTest):
         self.assertIn("http", major_details['major_home_url'])
         self.assertEqual(major_details['gpa_2yr'][0]['count'], 0)
 
+
 class TestCoiApi(ApiTest):
     def setUp(self):
         super(TestCoiApi, self).setUp()
@@ -36,12 +37,12 @@ class TestCoiApi(ApiTest):
         curric_coi = json.loads(response.content)
         self.assertEqual(len(curric_coi), 1)
         self.assertEqual(curric_coi[0]['curric_name'], "TRAINING")
-        self.assertEqual(curric_coi[0]['coi'], 1.9)
+        self.assertEqual(curric_coi[0]['score'], 1.9)
 
     def test_course(self):
         response = CourseCOI.as_view()(self.request,
                                        department_abbrev='TRAIN')
         course_coi = json.loads(response.content)
-        self.assertEqual(len(course_coi), 33)
-        self.assertEqual(course_coi[0]['coi'], 2.1)
+        self.assertEqual(len(course_coi), 19)
+        self.assertEqual(course_coi[0]['score'], 2.1)
         self.assertEqual(course_coi[0]['course_id'], "TRAIN 1")
