c4ee5575b3b982826468e158fd0a0885dd2677ee
Stephen De Vight <devights@uw.edu>
2025-02-14 13:28:29 -0800

    test debug views

--- pathways/tests/test_views.py
+++ pathways/tests/test_views.py
@@ -4,8 +4,13 @@
 from django.test import RequestFactory, TestCase
 from django.contrib.auth.models import User
 from django.contrib.sessions.middleware import SessionMiddleware
+from django.urls import reverse
+from django.urls.exceptions import NoReverseMatch
+from django.conf import settings
 from pathways.views.pages import DefaultPageView
+from importlib import reload
 import mock
+import sys
 
 
 class PagesViewTest(TestCase):
@@ -28,3 +33,18 @@ class PagesViewTest(TestCase):
         with self.settings(LIMIT_USER_ACCESS=True):
             response = DefaultPageView.as_view()(self.request)
             self.assertEqual(response.status_code, 401)
+
+    @mock.patch('pathways.views.is_member_of_group', return_value=True)
+    def test_is_member(self, im):
+        with self.settings(LIMIT_USER_ACCESS=True):
+            response = DefaultPageView.as_view()(self.request)
+            self.assertEqual(response.status_code, 200)
+
+    def test_debug_urls(self):
+        with self.settings(DEBUG=True):
+            reload(sys.modules['pathways.urls'])
+            reload(sys.modules['project.urls'])
+            try:
+                reverse('500_response')
+            except NoReverseMatch:
+                self.fail('500 reverse not found')
