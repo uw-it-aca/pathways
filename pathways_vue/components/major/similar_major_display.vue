6363165ca97117b2698d2f2d087ad1cc9218d999
leilalmw <ml253@uw.edu>
2025-06-30 14:09:08 -0700

    UI changes added to file similar_major_display

--- pathways_vue/components/major/similar_major_display.vue
+++ pathways_vue/components/major/similar_major_display.vue
@@ -1,7 +1,12 @@
 <template>
-  <h3><a :href="major_url">{{majorData.credential_title}}</a></h3>
-  <p>{{majorData.major_school}} - {{majorData.campus}}</p>
-  <major-capacity-display :admissionType="majorData.major_admission" />
+  <div class="my-2 py-2" style="font-family: 'Open Sans'; border-bottom: 1px solid rgba(0, 0, 0, 0.1);">
+    <h3 class="fw-bold" style="font-size: 16px;">
+    <a :href="major_url" class="text-decoration-none">{{majorData.credential_title}}</a>
+    </h3>
+    <p class="text-uppercase text-secondary mb-1" style="font-size: 14px;">{{majorData.major_school}} - {{majorData.campus}}
+    </p>
+    <major-capacity-display :admissionType="majorData.major_admission" />
+  </div>
 </template>
 
 <script>
