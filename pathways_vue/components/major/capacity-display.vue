0e7874c1d72d709d68f0f405bb180df443d64b28
leilalmw <ml253@uw.edu>
2025-06-30 14:10:48 -0700

    UI changes added to file capacity-disolay

--- pathways_vue/components/major/capacity-display.vue
+++ pathways_vue/components/major/capacity-display.vue
@@ -1,46 +1,48 @@
 <template>
-<div><i :class="capacityIcon"></i>{{capacityText}}</div>
-</template>
+  <div style="font-size: 16px;">
+  <i :class="capacityIcon" style = "color:#4E2F7F; margin-right: 8px; font-size: 14px;">
+  </i>{{capacityText}}</div>
+  </template>
 
-<script>
+  <script>
 
-export default {
-  name: "MajorCapacityDisplay",
-  props: {
-    admissionType: {
-      type: String,
-      required: true,
+  export default {
+    name: "MajorCapacityDisplay",
+    props: {
+      admissionType: {
+        type: String,
+        required: true,
+      },
     },
-  },
-  data() {
-    return {
-    };
-  },
-  methods: {
-  },
-  computed: {
-    capacityText(){
-      if(this.admissionType === "open"){
-        return "Open";
-      } else if(this.admissionType === "minimumRequirements"){
-        return "Minimum Requirements";
-      } else if(this.admissionType === "capacity-constrained"){
-        return "Capacity Constrained";
-      } else {
-        return "Unknown Requirements";
-      }
+    data() {
+      return {
+      };
     },
-    capacityIcon(){
-      if(this.admissionType === "open"){
-        return "bi-circle";
-      } else if(this.admissionType === "minimumRequirements"){
-        return "bi-record-circle";
-      } else if(this.admissionType === "capacity-constrained"){
-        return "bi-circle-fill";
-      } else {
-        return "bi-question-circle";
-      }
+    methods: {
     },
-  },
-};
-</script>
+    computed: {
+      capacityText(){
+        if(this.admissionType === "open"){
+          return "Open";
+        } else if(this.admissionType === "minimumRequirements"){
+          return "Minimum Requirements";
+        } else if(this.admissionType === "capacity-constrained"){
+          return "Capacity Constrained";
+        } else {
+          return "Unknown Requirements";
+        }
+      },
+      capacityIcon(){
+        if(this.admissionType === "open"){
+          return "bi-circle";
+        } else if(this.admissionType === "minimumRequirements"){
+          return "bi-record-circle";
+        } else if(this.admissionType === "capacity-constrained"){
+          return "bi-circle-fill";
+        } else {
+          return "bi-question-circle";
+        }
+      },
+    },
+  };
+  </script>
