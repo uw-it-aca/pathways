75302fac94b9b6e8803098f583358b4ec482069e
leilalmw <ml253@uw.edu>
2025-06-30 14:27:35 -0700

    styling without border, filter does not work

--- pathways_vue/components/major/similar_major_row.vue
+++ pathways_vue/components/major/similar_major_row.vue
@@ -1,10 +1,18 @@
 <template>
   <div v-if="is_program" class="my-2 py-2">
-      <h3 class="fw-bold" style="font-size: 16px;">{{majorData.program_title}}</h3>
-      <a href="#" v-on:click.prevent="expandChildren">
-      <i :class="expand_icon"></i>
-    </a>
-    <p>{{majorData.program_school}} - {{majorData.program_campus}}</p>
+
+    <div class="row">
+      <div class="col-11">
+        <h3 class="fw-bold" style="font-size: 16px;">{{majorData.program_title}}</h3>
+        <p>{{majorData.program_school}} - {{majorData.program_campus}}</p>
+      </div>
+      <div class="col-1 text-center h3">
+        <a href="#" v-on:click.prevent="expandChildren">
+          <i :class="expand_icon"></i>
+        </a>
+      </div>
+    </div>
+
     <div v-if="expanded" style="margin-left: 40px;">
       <similar-major-display
         v-for="(major, index) in majorData.program_majors"
@@ -55,7 +63,7 @@ export default {
       return this.majorData.program_majors !== undefined;
     },
     expand_icon() {
-      return this.expanded ? "bi-chevron-compact-up" : "bi-chevron-compact-down"
+      return this.expanded ? "bi-chevron-down" : "bi-chevron-right"
     }
   },
 };
