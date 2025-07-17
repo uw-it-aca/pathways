addc726ffed6102f17d5be675cf1e26d6d9d2fd8
Charlon Palacay <charlon@gmail.com>
2025-07-17 10:44:26 -0700

    Reverts to django 4.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=[
-        "django~=5.2",
+        "django~=4.2",
         "UW-Django-SAML2~=1.8",
         'lxml==4.9.4',
         "whoosh~=2.7",
