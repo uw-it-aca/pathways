61131aff9f0443706f8bf3e0d3d6c58943f81b6d
Charlon Palacay <charlon@gmail.com>
2026-01-23 12:00:55 -0800

    Update pkgs and layout.

--- pathways_vue/layout.vue
+++ pathways_vue/layout.vue
@@ -39,56 +39,7 @@
       <Feedback />
     </template>
 
-    <template #footer>
-      <!-- footer section override -->
-      <!--<div class="bg-dark">
-        <div class="container-xl">
-          <div class="text-white font-weight-light py-3 small">
-            <div class="d-flex justify-content-between">
-              <ul class="list-inline mb-2">
-                <li class="list-inline-item item-dot">
-                  <a
-                    href="mailto:help@uw.edu?subject=DawgPath Help, please route to Student and Instructor Success Analytics Service Offering"
-                    title="Contact the DawgPath team"
-                    class="text-white text-decoration-none me-2"
-                    ><i class="bi bi-envelope-fill me-0"></i> Contact</a
-                  >
-                </li>
-                <li class="list-inline-item item-dot">
-                  <a
-                    href="https://itconnect.uw.edu/tools-services-support/teaching-learning/research/success-analytics/apps/#dawg"
-                    title="DawgPath Docs on IT Connect"
-                    class="text-white text-decoration-none me-2"
-                    >Documentation</a
-                  >
-                </li>
-                <li class="list-inline-item item-dot">
-                  <a
-                    href="https://www.washington.edu/online/terms/"
-                    title="UW Terms of Use"
-                    class="text-white text-decoration-none me-2"
-                    >Terms</a
-                  >
-                </li>
-                <li class="list-inline-item">
-                  <a
-                    href="https://www.washington.edu/online/privacy/"
-                    title="UW Privacy Policy"
-                    class="router-link text-white text-decoration-none me-2"
-                    >Privacy</a
-                  >
-                </li>
-              </ul>
-              <Feedback />
-            </div>
-
-            <div class="font-weight-light" style="color: #aaa">
-              &copy; {{ new Date().getFullYear() }} University of Washington
-            </div>
-          </div>
-        </div>
-      </div>-->
-    </template>
+    <template #footer></template>
   </STopbarNeo>
 </template>
 
