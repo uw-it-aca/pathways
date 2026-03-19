817c88f36d8322aff8972edcd4708379a22719dd
Charlon Palacay <charlon@gmail.com>
2026-03-18 19:30:01 -0700

    Updates temmplatetags test.

--- pathways/tests/test_templatetags.py
+++ pathways/tests/test_templatetags.py
@@ -3,34 +3,32 @@
 
 import re
 from django.test import TestCase
-from pathways.templatetags.vite import vite_styles, vite_scripts
+from app_name.templatetags.vite import vite_styles, vite_scripts
 from pathways.templatetags.hashing_tag import hash_netid
 
 
 class ViteTestClass(TestCase):
+    def setUp(self):
+        # Setup run before every test method.
+        pass
+
+    def tearDown(self):
+        # Clean up run after every test method.
+        pass
+
     def test_vite_styles(self):
-        entries = ("pathways_vue/main.js",)
+        entries = ("app_name_vue/main.js",)
         link = vite_styles(*entries)
-        pattern = re.compile(
-            '<link rel="[\\w]*" href="[\\w\\D]*main.[\\d\\w]*.css" />'
-        )
-        self.assertTrue(pattern.match(link))
+        pattern = re.compile(r'<link\s+[^>]*href="[^"]*main-[^"]*"[^>]*>')
+        self.assertTrue(pattern.search(link))
 
     def test_vite_scripts(self):
-        entries = ("pathways_vue/main.js",)
+        entries = ("app_name_vue/main.js",)
         script = vite_scripts(*entries)
         pattern = re.compile(
-            '<script type="[\\w]*" src="[\\w\\D]*main.[\\d\\w]*.js"></script>'
+            r'<script\s+[^>]*src="[^"]*main-[^"]*"[^>]*></script>'
         )
-        self.assertTrue(pattern.match(script))
-
-    def test_manifest_names(self):
-        entries = ("pathways_vue/main.js", "pathways_vue/main.js")
-        try:
-            vite_scripts(*entries)
-        except Exception:
-            self.fail("Didn't ignore duplicate entry")
-
+        self.assertTrue(pattern.search(script))
 
 class HashTestClass(TestCase):
     def test_hash_netid(self):
