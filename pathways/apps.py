71da16ad832dfdaac50207f84af45242d5f446bd
Charlon Palacay <charlon@gmail.com>
2026-03-26 13:49:33 -0700

    Adds missing staticFiles config.

--- pathways/apps.py
+++ pathways/apps.py
@@ -2,7 +2,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from django.apps import AppConfig
+from django.contrib.staticfiles.apps import StaticFilesConfig
 
+
+class ViteStaticFilesConfig(StaticFilesConfig):
+    ignore_patterns = ['CVS', '*~']
 
 class PathwaysConfig(AppConfig):
     name = 'pathways'
