8e43bb37352ede40517bfa53f9d6423c7b77fbd6
Charlon Palacay <charlon@gmail.com>
2026-04-21 11:02:51 -0700

    Fixes app start script.

--- docker/app_start.sh
+++ docker/app_start.sh
@@ -2,10 +2,11 @@
 
 python3 manage.py migrate
 python3 manage.py import_data
-python3 manage.py build_search_index
 
 if [ "$ENV"  = "localdev" ]
 then
-    # Add any other localdev specific commands here
-    :
+
+  python manage.py build_search_index
+
+
 fi
