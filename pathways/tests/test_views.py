c30a71b3fbb79be95cdb9e108ac699ae34d8ee80
Charlon Palacay <charlon@gmail.com>
2026-04-21 14:43:12 -0700

    Fix test.

--- pathways/tests/test_views.py
+++ pathways/tests/test_views.py
@@ -1,20 +1,22 @@
 # Copyright 2026 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-from django.test import RequestFactory, TestCase
+import sys
+from importlib import reload
+
+import mock
 from django.contrib.auth.models import User
 from django.contrib.sessions.middleware import SessionMiddleware
+from django.test import RequestFactory, TestCase
 from django.urls import reverse
 from django.urls.exceptions import NoReverseMatch
+
 from pathways.views.pages import DefaultPageView
-from importlib import reload
-import mock
-import sys
 
 
 class PagesViewTest(TestCase):
     def setUp(self):
-        self.request = RequestFactory().get('/')
+        self.request = RequestFactory().get("/")
         self.request.user = User()
         get_response = mock.MagicMock()
         middleware = SessionMiddleware(get_response)
@@ -25,15 +27,17 @@ class PagesViewTest(TestCase):
         with self.settings(GOOGLE_ANALYTICS_KEY=" "):
             response = DefaultPageView.as_view()(self.request)
             self.assertIsInstance(response.context_data, dict)
-            self.assertEqual(response.context_data.get('ga_key'), ' ')
-            self.assertEqual(response.context_data.get('django_debug'), False)
+            self.assertEqual(
+                response.context_data.get("googleAnalyticsKey"), " "
+            )
+            self.assertEqual(response.context_data.get("debugMode"), False)
 
     def test_auth(self):
         with self.settings(LIMIT_USER_ACCESS=True):
             response = DefaultPageView.as_view()(self.request)
             self.assertEqual(response.status_code, 401)
 
-    @mock.patch('pathways.views.is_member_of_group', return_value=True)
+    @mock.patch("pathways.views.is_member_of_group", return_value=True)
     def test_is_member(self, im):
         with self.settings(LIMIT_USER_ACCESS=True):
             response = DefaultPageView.as_view()(self.request)
@@ -41,9 +45,9 @@ class PagesViewTest(TestCase):
 
     def test_debug_urls(self):
         with self.settings(DEBUG=True):
-            reload(sys.modules['pathways.urls'])
-            reload(sys.modules['project.urls'])
+            reload(sys.modules["pathways.urls"])
+            reload(sys.modules["project.urls"])
             try:
-                reverse('500_response')
+                reverse("500_response")
             except NoReverseMatch:
-                self.fail('500 reverse not found')
+                self.fail("500 reverse not found")
