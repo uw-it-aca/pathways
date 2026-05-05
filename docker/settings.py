7ff18469a028497f90aa3feac1c25ae94f9c6b54
Jim Laney <jlaney@uw.edu>
2026-05-05 13:33:04 -0700

    django-container 3.1.1

--- docker/settings.py
+++ docker/settings.py
@@ -31,13 +31,8 @@ DATABASES["uw_person"] = {
     "NAME": os.getenv("UW_PERSON_DB_NAME", "postgres"),
     "USER": os.getenv("UW_PERSON_DB_USER", "postgres"),
     "PASSWORD": os.getenv("UW_PERSON_DB_PASSWORD", "postgres"),
-    "OPTIONS": {
-        "pool": {
-            "min_size": int(os.getenv("UW_PERSON_DB_POOL_MIN", 1)),
-            "max_size": int(os.getenv("UW_PERSON_DB_POOL_MAX", 4)),
-        },
-    },
 }
+
 DATABASE_ROUTERS = ["uw_person_client.routers.UWPersonRouter"]
 
 # If you have file data, define the path here
