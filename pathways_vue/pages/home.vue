11be75aa44906d71421306d9e9798a3d2b0ca4ae
Charlon Palacay <charlon@gmail.com>
2026-07-14 10:32:59 -0700

    Display student data in json.

--- pathways_vue/pages/home.vue
+++ pathways_vue/pages/home.vue
@@ -15,58 +15,7 @@
       <div class="row">
         <div class="col">
           <BCard class="bg-body-tertiary rounded-3" border-variant="0">
-            <div class="row">
-              <div class="col">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aenean eu auctor nunc, ut malesuada risus. Proin hendrerit ac
-                  dolor eleifend fermentum. Vivamus commodo sed nisi vitae
-                  vestibulum. Praesent facilisis ac est sit amet vulputate. Cras
-                  sollicitudin ornare sapien a molestie. Donec nunc mauris,
-                  interdum quis scelerisque et, lobortis quis odio. Curabitur
-                  ultricies quis augue at posuere. Proin pretium sed nulla sit
-                  amet feugiat. Nunc justo dolor, blandit ac lorem ac, tempus
-                  consectetur magna. Praesent eu tempus massa, eu commodo nisi.
-                  Pellentesque a nisi non metus finibus cursus. Vestibulum
-                  consectetur, lacus a ultrices mattis, libero lacus ornare
-                  nibh, et mollis dui massa non nulla. Cras rhoncus nibh id
-                  feugiat tristique. Duis quis maximus augue. Cras quis
-                  tincidunt libero.
-                </p>
-              </div>
-              <div class="col">
-                <p>
-                  Maecenas placerat varius nulla at porta. Pellentesque leo
-                  mauris, egestas nec fringilla in, maximus eget lorem. Maecenas
-                  ultricies vitae libero porta iaculis. Nullam convallis est vel
-                  quam gravida suscipit. Integer mattis odio est, ut rhoncus
-                  neque malesuada vel. Nullam mollis posuere turpis.
-                  Pellentesque eu lacinia eros. Sed faucibus justo id libero
-                  iaculis, accumsan interdum turpis feugiat. Proin eu orci
-                  bibendum, mollis sapien at, laoreet risus. Ut id sapien vel
-                  enim commodo accumsan. Donec eget massa ut nibh dapibus
-                  vulputate. Ut non massa mollis erat sodales dapibus quis quis
-                  quam. In rutrum eu augue id semper. Donec sem augue, eleifend
-                  ac mi at, porttitor congue ipsum.
-                </p>
-              </div>
-              <div class="col">
-                <p>
-                  Maecenas placerat varius nulla at porta. Pellentesque leo
-                  mauris, egestas nec fringilla in, maximus eget lorem. Maecenas
-                  ultricies vitae libero porta iaculis. Nullam convallis est vel
-                  quam gravida suscipit. Integer mattis odio est, ut rhoncus
-                  neque malesuada vel. Nullam mollis posuere turpis.
-                  Pellentesque eu lacinia eros. Sed faucibus justo id libero
-                  iaculis, accumsan interdum turpis feugiat. Proin eu orci
-                  bibendum, mollis sapien at, laoreet risus. Ut id sapien vel
-                  enim commodo accumsan. Donec eget massa ut nibh dapibus
-                  vulputate. Ut non massa mollis erat sodales dapibus quis quis
-                  quam. In rutrum eu augue id semper. Donec sem augue, eleifend
-                  ac mi at, porttitor congue ipsum.
-                </p>
-              </div>
-            </div>
+            <pre>{{ JSON.stringify(contextStore.context.personData, null, 2) }}</pre>
           </BCard>
         </div>
       </div>
@@ -79,6 +28,7 @@
   import SearchMini from "@/components/search/search-mini.vue";
   import { useCustomFetch } from "@/composables/customFetch";
   import { BCard } from "bootstrap-vue-next";
+  import { useContextStore } from "@/stores/context";
 
   export default {
     name: "HomeComp",
@@ -93,7 +43,15 @@
         welcomeModal: null,
       };
     },
-    mounted() {},
+    computed: {
+      contextStore() {
+        return useContextStore();
+      },
+    },
+    mounted() {
+      const contextStore = useContextStore();
+      console.log("Context store data:", contextStore.context);
+    },
     methods: {
       showWelcomeModal() {
         this.welcomeModal = new Modal(
